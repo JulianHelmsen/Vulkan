3b2086c0273bed92b54b9849139fb285db01542c
JulianHelmsen <juliandominikhelmsen@gmail.com>
2022-08-14 20:19:37 +0200

    drawing a triangle

--- sandbox/res/vertex_shader.glsl
+++ sandbox/res/vertex_shader.glsl
@@ -1,9 +1,10 @@
 #version 450
 
-layout(location = 0) in vec3 position;
 
 vec2 positions[3] = vec2[](
-	vec2(0.0f, 0.0f), vec2(1.0f, 1.0f), vec2(1.0f, 0.0f)
+    vec2(0.0, -0.5),
+    vec2(0.5, 0.5),
+    vec2(-0.5, 0.5)
 );
 
 void main() {
