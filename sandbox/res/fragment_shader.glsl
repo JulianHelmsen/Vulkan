ada867965c3457210e91e45bc3c35f282dce3e35
JulianHelmsen <juliandominikhelmsen@gmail.com>
2022-08-23 13:53:51 +0200

    added push constants

--- sandbox/res/fragment_shader.glsl
+++ sandbox/res/fragment_shader.glsl
@@ -2,6 +2,9 @@
 
 layout(location = 0) out vec4 out_color;
 
+
+layout(location = 0) in vec3 f_world_pos;
+
 void main() {
-	out_color = vec4(1.0f, 1.0f, 0.0f, 1.0f);
+	out_color = vec4(f_world_pos, 1.0f);
 }
--- sandbox/res/vertex_shader.glsl
+++ sandbox/res/vertex_shader.glsl
@@ -1,12 +1,18 @@
 #version 450
 
 layout(location = 0) in vec3 position;
-vec2 positions[3] = vec2[](
-    vec2(0.0, -0.5),
-    vec2(0.5, 0.5),
-    vec2(-0.5, 0.5)
-);
+
+layout(push_constant) uniform camera_object{
+    mat4 view_projection_matrix;
+    mat4 model_matrix;
+
+}camera;
+
+layout(location = 0) out vec3 f_world_pos;
+
 
 void main() {
-    gl_Position = vec4(position, 1.0f);//vec4(positions[gl_VertexIndex], 0.0f, 1.0f);
+    vec4 world_pos = camera.model_matrix * vec4(position, 1.0f);
+    f_world_pos = world_pos.xyz;
+    gl_Position = camera.view_projection_matrix * world_pos;
 }
